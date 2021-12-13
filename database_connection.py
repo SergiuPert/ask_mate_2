3efe1fa1c79b917706854b486b575fe9797734fa
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2021-12-13 10:41:22 +0200

    "Modified .env.template and database_connection.py"

--- database_connection.py
+++ database_connection.py
@@ -1,32 +1,52 @@
-import pymysql
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+import psycopg2
+import psycopg2.extras
+
+
+def get_connection_string():
+    # setup connection string
+    # to do this, please define these environment variables first
+
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    port = os.environ.get('PORT')
+    database_name = os.environ.get('PSQL_DB_NAME')
+
+    env_variables_defined = user_name and password and host and database_name
+
+    if env_variables_defined:
+        # this string describes all info for psycopg2 to connect to the database
+        return 'postgresql://{user_name}:{password}@{host}:{PORT}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            port=port,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
 
 def open_database():
     try:
-        connection = pymysql.connect(
-            host=os.getenv("HOST"),
-            port=os.getenv("PORT"),
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            db=os.getenv("DB"),
-        )
-    except pymysql.DatabaseError as exception:
-        print("Database connection problem")
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
         raise exception
-
     return connection
 
 
 def connection_handler(function):
     def wrapper(*args, **kwargs):
         connection = open_database()
-        cursor = connection.cursor(pymysql.cursors.DictCursor)
-        ret_value = function(cursor, *args, **kwargs)
-        connection.commit()
+        # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
         connection.close()
         return ret_value
 
